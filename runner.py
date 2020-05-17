feb9a304114ff4bda8e9bff32edac2a2c2144b70
tjw1184 <tjw1184@yahoo.com>
2020-05-17 11:56:59 -0700

    update to new 429 fix branch

--- runner.py
+++ runner.py
@@ -44,22 +44,27 @@ def main() -> None:
             shutil.copyfile("/youtubedl/origconfigs/youtube-dl-channels.txt","/configs/youtube-dl-channels.txt")                     
         
         # Dirty hack to implement the 429 error workaround provided by colethedj, lock to 3-8-20 branch for now
-         # https://gitlab.com/colethedj/youtube-dl-429-patch
+        # https://gitlab.com/colethedj/youtube-dl-429-patch
         prevdir = os.getcwd()
         os.chdir("/temp")
-        run(["/usr/bin/git","clone","https://github.com/ytdl-org/youtube-dl.git","--depth","1"])
-        run(["/usr/bin/git","clone","https://gitlab.com/colethedj/youtube-dl-429-patch.git"])
-        os.chdir("/temp/youtube-dl/youtube_dl")
-        run(["/usr/bin/git","apply","../../youtube-dl-429-patch/youtube_dl_429.patch"])
-        os.chdir("/temp/youtube-dl")
+        
+        # Updated youtube-dl broke the patch, so had to clone and hack the whole branch
+        #run(["/usr/bin/git","clone","https://github.com/ytdl-org/youtube-dl.git","-b","2020.03.08","--depth","1"])
+        #run(["/usr/bin/git","clone","https://gitlab.com/colethedj/youtube-dl-429-patch.git"])
+        #os.chdir("/temp/youtube-dl/youtube_dl")
+        #run(["/usr/bin/git","apply","../../youtube-dl-429-patch/youtube_dl_429.patch"])
+        #os.chdir("/temp/youtube-dl")
+        
+        run(["/usr/bin/git","clone","https://github.com/tjw1184/youtube-dl-429.git","-b","2020.05.08.429fix","--depth","1"])
+        os.chdir("/temp/youtube-dl-429")
         run(["/usr/local/bin/pip3","install","."])
         os.chdir(prevdir)
         
-        #open bash shell
-        run(["/bin/bash"])
-        
         # re-enable pip install when hack no longer needed
         #run(["pip", "install", "--upgrade", "youtube-dl"])
+        
+        #open bash shell
+        run(["/bin/bash"])
         
         # run youtubedl every interval seconds
 #        run(["/usr/local/bin/youtube-dl", "--config-location", "/configs/youtube-dl.conf"])
